7c87b412fc22555f851439bb03119fe5a16ee43a
mricoism <mrikoismail73@gmail.com>
2024-10-26 12:59:08 +0700

    add project CounterMVVM

---
+++ CounterMVVM-app/app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.countermvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.countermvvm.ui.theme.CounterMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterMVVMTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//
+//                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TheCounterApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TheCounterApp() {
+    var count = remember { mutableStateOf(0) }
+
+    val textStyle =
+        TextStyle(fontFamily = FontFamily.Monospace, fontSize = 24.sp, color = Color(83, 100, 147))
+    val buttonColor = IconButtonColors(
+        contentColor = Color.White,
+        containerColor = Color(83, 100, 147),
+        disabledContentColor = Color.White,
+        disabledContainerColor = Color.DarkGray
+    )
+
+    fun increment() {
+        count.value++
+    }
+
+    fun decrement() {
+        count.value--
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Count: ${count.value}", style = textStyle)
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            IconButton(colors = buttonColor,
+                onClick = { decrement() }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Decrement"
+                )
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            IconButton(colors = buttonColor,onClick = { increment() }) {
+                Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = "Increment")
+            }
+
+
+        }
+    }
+
+}
