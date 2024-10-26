4b0c5da6061d19d7ee5223d61099c571ac66a3f7
mricoism <mrikoismail73@gmail.com>
2024-10-26 16:21:32 +0700

    CounterMVVM left behind

--- CounterMVVM-app/app/src/main/java/com/example/countermvvm/CounterViewModel.kt
+++ CounterMVVM-app/app/src/main/java/com/example/countermvvm/CounterViewModel.kt
@@ -4,16 +4,24 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel: ViewModel() {
-    private val _count = mutableStateOf(0)
+class CounterViewModel(
+//    private val counterRepository: CounterRepository // Di Project besar menggunakan ini. Tapi karna latihan jadi di gantikan dengan membuat direct object di dalam CounterViewModel
+): ViewModel() {
+    private val _repository: CounterRepository = CounterRepository() // Direct make object
+//    private val _count = mutableStateOf(0)
+private val _count = mutableStateOf(_repository.getCounter().count)
 
     val count: MutableState<Int> = _count
 
     fun increment() {
-        _count.value++
+//        _count.value++
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement() {
-        _count.value--
+//        _count.value--
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 }
