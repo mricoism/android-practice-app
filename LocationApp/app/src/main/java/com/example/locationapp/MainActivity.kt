c36a980b9c6a38598346aacfb37ac1fe18881838
mricoism <mrikoismail73@gmail.com>
2024-10-31 15:24:11 +0700

    Adding project LocationApp

---
+++ LocationApp/app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.example.locationapp
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.locationapp.ui.theme.LocationAppTheme
+import com.example.locationapp.ui.theme.LocationUtils
+import android.Manifest
+import android.widget.Toast
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.locationapp.ui.theme.LocationViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val viewModel: LocationViewModel = viewModel()
+            LocationAppTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//
+//                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp(viewModel)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(viewModel: LocationViewModel) {
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+    LocationDisplay(locationUtils = locationUtils, viewModel, context = context)
+}
+
+
+@Composable
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    viewModel: LocationViewModel,
+    context: Context
+) {
+    val location = viewModel.location.value
+    val address = location?.let {
+        locationUtils.reverseGeocodeLocation(location)
+    }
+
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) { permission ->
+            if (permission[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permission[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                // I have access to location
+                locationUtils.requestLocationUpdates(viewModel = viewModel)
+            } else {
+                // Ask For permission
+                val rationalRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+
+                if (rationalRequired) {
+                    Toast.makeText(
+                        context,
+                        "Location is required for this feature to work",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Location is required, PLEASE enable in android setting",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        if (location != null) {
+            Text(text = "Address: ${location.latitude} | ${location.longitude} \n $address")
+//            Text(text = "Address: ${address}")
+        } else {
+            Text(text = "Location Not Available")
+        }
+
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                // Permission Already Granted update location
+                locationUtils.requestLocationUpdates(viewModel = viewModel)
+            } else {
+                // Request location permission
+                requestPermissionLauncher.launch(arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ))
+            }
+        }) {
+            Text(text = "Get LOocation")
+        }
+    }
+}
---
+++ LocationApp/app/src/main/java/com/example/locationapp/ui/theme/LocationData.kt
@@ -0,0 +1,6 @@
+package com.example.locationapp.ui.theme
+
+data class LocationData(
+    val latitude: Double,
+    val longitude: Double
+)
