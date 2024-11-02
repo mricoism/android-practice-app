6bccd152e5045520db9f33cd7cef63ed2afeb9ed
mricoism <mrikoismail73@gmail.com>
2024-11-02 21:46:48 +0700

    add progress learn view binding

---
+++ learnkotlincoroutines/app/src/main/java/com/example/learn_kotlin_coroutines/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.learn_kotlin_coroutines
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
