50fa99fed72b79da04d7b47d552fbe8a9c90d61a
mricoism <mrikoismail73@gmail.com>
2024-11-04 22:42:26 +0700

    Finishing EncryptedSharedPreferencesApp

--- EncryptedSharedPreferencesApp/app/src/main/java/com/mricoism/encryptedsharedpreferencesapp/MainActivity.kt
+++ EncryptedSharedPreferencesApp/app/src/main/java/com/mricoism/encryptedsharedpreferencesapp/MainActivity.kt
@@ -1,4 +1,57 @@
 package com.mricoism.encryptedsharedpreferencesapp
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import androidx.security.crypto.MasterKeys
+import com.mricoism.encryptedsharedpreferencesapp.databinding.ActivityMainBinding
+
+
+class MainActivity: AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // https://stackoverflow.com/questions/62498977/how-to-create-masterkey-after-masterkeys-deprecated-in-android
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            "preferences",
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        val name = sharedPreferences.getString("name", "")
+        val age = sharedPreferences.getString("age", "")
+
+        binding.idEdtName.setText(name)
+        binding.idEdtAge.setText(age)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            "preferences",
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        sharedPreferences.edit().putString("name", binding.idEdtName.text.toString()).apply()
+        sharedPreferences.edit().putString("age", binding.idEdtName.text.toString()).apply()
+    }
 }
--- EncryptedSharedPreferencesApp/readMe.md
+++ EncryptedSharedPreferencesApp/readMe.md
@@ -1,3 +1,8 @@
 
 Source Learn:
 https://www.geeksforgeeks.org/encrypted-shared-preferences-in-android/
+https://www.geeksforgeeks.org/how-to-view-data-stored-in-shared-preferences-in-android-studio/
+
+
+TODO CHALLENGE:
+Retrieve and show it. Means maybe you need to decrypt it too
