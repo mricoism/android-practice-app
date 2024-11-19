ee7b7e2d0876cde9b2d2def6823bb6e48fec79a5
mricoism <mrikoismail73@gmail.com>
2024-11-19 22:23:01 +0700

    Finish FragmentApp

--- FragmentApp/app/src/main/java/com/mricoism/fragmentapp/MainActivity.kt
+++ FragmentApp/app/src/main/java/com/mricoism/fragmentapp/MainActivity.kt
@@ -16,6 +16,44 @@ class MainActivity: AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        // Set initial fragment
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit() // To Apply above changes
+        }
+
+        /*
+        IMPORTANT!
+        If we click back button on device. Then our activity will close.
+        because those fragments currently don't have their own stack.
+        So when ever we replace such fragment here, then we don't push that fragment on top of the fragment or activities stack.
+        So we can navigate back, with button back device to the fragment we navigated to previously.
+         */
+
+        binding.btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+
+                // we can navigate back, with button back device to the fragment we navigated to previously.
+                addToBackStack(null) // its take a name as a parameter to later detect which fragment we push on that back stack, So to give this fragment a unique name on our back stack. But we can pass null if we dont care about the name.
+
+                commit() // To Apply above changes
+            }
+        }
+
+        binding.btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit() // To Apply above changes
+            }
+        }
+
+
+
     }
 }
 
