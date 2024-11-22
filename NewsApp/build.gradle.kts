735ec846a0eb4e0c3cd42df265af962428fda82a
mricoism <mrikoismail73@gmail.com>
2024-11-22 16:58:13 +0700

    Setup layout NewsApp

--- NewsApp/build.gradle.kts
+++ NewsApp/build.gradle.kts
@@ -1,5 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.4")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
