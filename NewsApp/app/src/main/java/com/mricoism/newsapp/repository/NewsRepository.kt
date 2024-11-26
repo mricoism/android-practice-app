c7c30466058a77687d7fe8977cac841df151ad19
mricoism <mrikoismail73@gmail.com>
2024-11-26 10:00:31 +0700

    Update progress NewsApp 3

--- NewsApp/app/src/main/java/com/mricoism/newsapp/repository/NewsRepository.kt
+++ NewsApp/app/src/main/java/com/mricoism/newsapp/repository/NewsRepository.kt
@@ -1,6 +1,8 @@
 package com.mricoism.newsapp.repository
 
+import com.mricoism.newsapp.api.RetrofitInstance
 import com.mricoism.newsapp.db.ArticleDatabase
+import com.mricoism.newsapp.models.Article
 
 /*
 what's the general meaning of repository ?
@@ -8,5 +10,15 @@ it means a place or a container where something is deposited or stored
  */
 class NewsRepository(val db: ArticleDatabase) {
 
+    suspend fun getHeadlines(countryCode: String, pageNumber: Int) =
+        RetrofitInstance.api.getHeadlines(countryCode, pageNumber)
 
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getFavouriteNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
