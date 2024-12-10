f88e572cacfb66b596b04922f6a430e80198d696
mricoism <mrikoismail73@gmail.com>
2024-12-10 22:12:24 +0700

    add project AppShellWithAARGroovyDSL

--- AppShellWithAARGroovyDSL/app/src/main/java/com/mricoism/appshellwithaargroovydsl/MainActivity.kt
+++ AppShellWithAARGroovyDSL/app/src/main/java/com/mricoism/appshellwithaargroovydsl/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.mricoism.appshellwithaargroovydsl
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mricoism.appshellwithaargroovydsl.databinding.ActivityMainBinding
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "Move to Flutter app"
+        binding.button.setOnClickListener {
+            Log.d("HWS", "Button clicked")
+            startActivity(
+                FlutterActivity.withNewEngine().build(this)
+            )
         }
     }
 }
