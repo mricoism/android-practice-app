12bc1e4850a389a6a5706444051333db636d7356
mricoism <mrikoismail73@gmail.com>
2024-12-11 18:45:23 +0700

    integrate project with firebase crashlytics

--- copycatBAFBPKB/app/src/main/java/com/mricoism/copycatbafbpkb/MainActivity.kt
+++ copycatBAFBPKB/app/src/main/java/com/mricoism/copycatbafbpkb/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.mricoism.copycatbafbpkb
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mricoism.copycatbafbpkb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.text = "Trigger crash!"
+        binding.button.setOnClickListener {
+            Log.d("HWS", "setOnClickListener")
+            throw RuntimeException("Test Crash RIKO")
         }
     }
 }
